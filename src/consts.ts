79f827683e4e3806cec96a076d0bf69fbdf43844
Calvin Lee <jinwoo0451@naver.com>
2025-12-14 02:35:50 +0900

    feat: Add a new services page, update header navigation links, and modify the site title constant.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Chemono World';
+export const SITE_TITLE = '@chemonoworld';
 export const SITE_DESCRIPTION = 'Tech Blog of Jinwoo Lee';
