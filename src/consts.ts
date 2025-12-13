437a53c95ada89ccf6db5e220ca27dd0084a8616
Calvin Lee <jinwoo0451@naver.com>
2025-12-14 00:16:40 +0900

    o

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Chemono World';
-export const SITE_DESCRIPTION = 'Personal Blog of Jinwoo Lee';
+export const SITE_DESCRIPTION = 'Tech Blog of Jinwoo Lee';
